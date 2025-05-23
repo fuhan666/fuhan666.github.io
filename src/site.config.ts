967c7d9ae5b4b83575af2c7f6194bf9dad0bb99e
Han Fu <hanfu.hf@gmail.com>
2025-05-24 04:53:00 +0800

    chore: update waline server link

--- src/site.config.ts
+++ src/site.config.ts
@@ -123,7 +123,7 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: 'https://astro-theme-pure-waline.arthals.ink/',
+    server: 'https://waline-5dexqlkay-hanfuhf-gmailcoms-projects.vercel.app/',
     // Refer https://waline.js.org/en/guide/features/emoji.html
     emoji: ['bmoji', 'weibo'],
     // Refer https://waline.js.org/en/reference/client/props.html
